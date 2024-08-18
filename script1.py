0c1182c7103018de1a03e4a191bae3b87579da51
mihir-darji <darjimihir16@gmail.com>
2024-08-18 23:27:53 +0530

    setting up env variable dynamically

--- script1.py
+++ script1.py
@@ -5,3 +5,11 @@ name = os.getenv("NAME")
 print("name is equal to mihir: ", name=="mihir")
 
 print("THE ENVIRONMENT IS ",os.getenv("VARIABLE"))
+
+print("github_env_variable_1 value is ", os.getenv("github_env_variable_1"))
+
+print("github_env_variable_2 value is ", os.getenv("github_env_variable_2"))
+
+print("export1 value is ", os.getenv("export1"))
+
+print("export2 value is ", os.getenv("export2"))
