1fa2f8f5a46227735608c5dd7dbef6437703e1b8
mihir-darji <darjimihir16@gmail.com>
2024-08-19 18:14:36 +0530

    create workflow for artifact

--- script.py
+++ script.py
@@ -1,1 +1,11 @@
+import datetime
+
 print("hello world")
+
+print("generate artifact")
+
+print("all statement going to log into artifact file")
+
+print("artifact write sucessfully")
+
+print("bye", datetime.datetime.now())
